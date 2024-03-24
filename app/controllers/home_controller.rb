12610c7b29186755939395dc323637b375e06558
Annastacia Mumbua Kioko <anne@mradi.co.ke>
2024-03-24 17:21:10 +0300

    add geolocator

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,14 @@
 class HomeController < ApplicationController
+
+  before_action :get_user_location
+
+  def index
+  end
+
+  private
+
+  def get_user_location
+    @user_location = request.location
+    byebug
+  end
 end
