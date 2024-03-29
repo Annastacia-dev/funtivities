0f491ec6c886972d75fb7df7b256eeeb5f0f9402
Annastacia Mumbua Kioko <annetotoh@gmail.com>
2024-03-29 09:16:16 +0300

    fix maifest file error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,10 @@ class HomeController < ApplicationController
   def index
   end
 
+  def manifest
+    render file: "#{Rails.root}/manifest.json", content_type: 'application/json'
+  end
+
   private
 
   def get_user_location
